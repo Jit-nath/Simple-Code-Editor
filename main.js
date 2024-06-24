01947a2cc40c6ae874fd8a4bc142bdfb23a98d34
Jit Debnath <jitdebnathinkolkata@gmail.com>
2024-06-24 10:02:25 +0530

    made window-control functional

--- main.js
+++ main.js
@@ -3,25 +3,29 @@ function NewWindow() {
     const window = new BrowserWindow({
         minHeight: 600,
         minWidth: 900,
-        frame: false,
+        titleBarStyle: 'hidden',
+        titleBarOverlay: {
+            color: 'rgb(31, 31, 31)',
+            symbolColor: '#fff',
+            height: 30
+        },
         webPreferences: {
-            nodeIntegration: true, 
-            contextIsolation: false 
-        }
+            // preload: path.join(__dirname, 'preload.js'),
+            nodeIntegration: true,
+            contextIsolation: false
+        },
     });
     window.loadFile('index.html');
 }
 
 app.whenReady().then(NewWindow);
 
-// Quit when all windows are closed, except on macOS.
 app.on('window-all-closed', () => {
     if (process.platform !== 'darwin') {
         app.quit();
     }
 });
 
-// Recreate a window in the app when the dock icon is clicked and there are no other windows open (macOS).
 app.on('activate', () => {
     if (BrowserWindow.getAllWindows().length === 0) {
         NewWindow();
